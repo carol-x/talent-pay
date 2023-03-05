b8abe2187179711bdf561934b5627ff223fa5524
carolx <carolx@berkeley.edu>
2023-03-05 12:16:06 -0500

    formatting

--- contracts/PrivatePay.sol
+++ contracts/PrivatePay.sol
@@ -1,14 +1,23 @@
-IERC20 bob = IERC20(0xB0B195aEFA3650A6908f15CdaC7D92F8a5791B0B);
-IZkBobDirectDeposits queue = IZkBobDirectDeposits(0x668c5286eAD26fAC5fa944887F9D2F20f7DDF289);
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.9;
 
-bytes memory zkAddress = bytes("QsnTijXekjRm9hKcq5kLNPsa6P4HtMRrc3RxVx3jsLHeo2AiysYxVJP86mriHfN");
+import "@openzeppelin/contracts/token/erc20/ierc20.sol";
 
-address fallbackReceiver = msg.sender;
+contract PrivatePay {
 
-// Option A, through pool contract
-// Note that ether is an alias for 10**18 multiplier, as BOB token has 18 decimals
-bob.approve(address(queue), 100 ether);
-uint256 depositId = queue.directDeposit(fallbackReceiver, 100 ether, zkAddress);
+    IERC20 bob = IERC20(0xB0B195aEFA3650A6908f15CdaC7D92F8a5791B0B);
+    IZkBobDirectDeposits queue = IZkBobDirectDeposits(0x668c5286eAD26fAC5fa944887F9D2F20f7DDF289);
 
-// Option B, through ERC677 token interface
-bob.transferAndCall(address(queue), 100 ether, abi.encode(fallbackReceiver, zkAddress));
+    bytes zkAddress = bytes("QsnTijXekjRm9hKcq5kLNPsa6P4HtMRrc3RxVx3jsLHeo2AiysYxVJP86mriHfN");
+
+    address fallbackReceiver = msg.sender;
+
+    event payDeposited(uint amount, uint when);
+    
+    function privateDeposit() public {
+    // through ERC677 token interface
+    bob.transferAndCall(address(queue), msg.value, abi.encode(fallbackReceiver, zkAddress));
+    emit payDeposited(msg.value, block.timestamp);
+    }
+
+}
